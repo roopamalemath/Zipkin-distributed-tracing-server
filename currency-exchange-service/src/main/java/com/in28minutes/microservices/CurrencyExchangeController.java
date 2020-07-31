650bd228ee7bcb8e46de7b36813edd857d0fe2d9
roopa v malemath <roopa.malemath.dvg@gmail.com>
2020-07-31 23:30:39 +0530

    first commit

---
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/CurrencyExchangeController.java
@@ -0,0 +1,37 @@
+package com.in28minutes.microservices;
+
+import java.math.BigDecimal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	
+	private Logger logger=LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private Environment environment;
+	
+	@Autowired
+	CurrencyExchangeRepository currencyExchangeRepository;
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from,
+			                                   @PathVariable String to) {
+		//ExchangeValue exchangeValue=new ExchangeValue(1L, from, to, BigDecimal.valueOf(65));
+		ExchangeValue exchangeValue=currencyExchangeRepository.findByFromAndTo(from, to);
+		
+		// this will string back so converting into integer
+		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		logger.info("{}",exchangeValue);
+		return exchangeValue;
+	}
+
+}
